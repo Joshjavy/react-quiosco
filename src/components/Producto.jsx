ad8ee4a3efeaf7431fa03c22f418d9b52e8e0de0
Javier Dominguez Galindo <joshjavy@gmail.com>
2023-12-19 22:16:20 -0600

    270. Mostrando Imagen, Precio y Nombre del Producto

--- src/components/Producto.jsx
+++ src/components/Producto.jsx
@@ -1,6 +1,18 @@
 export default function Producto( { producto} ) {
-    const {} = producto
+    const {nombre,precio,imagen,categoria_id,id} = producto
   return (
-    <div>Producto</div>
+    <div className="border p-3 shadow bg-white">
+        <img 
+            alt={`imagen ${nombre}`}
+            className="w-full"
+            src={`/img/${imagen}.jpg`}
+        />
+
+        <div className="p-5">
+            <h3 className="text-2xl font-bold"> {nombre} </h3>
+            <p className="mt-5 font-black text-4xl text-amber-500"> {precio} </p>
+
+        </div>
+    </div>
   )
 }
