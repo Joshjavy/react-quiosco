f9eec27ced03f583949aa49a43a25c2d1f2db669
Javier Dominguez Galindo <joshjavy@gmail.com>
2023-12-21 17:51:56 -0600

    278. Resaltando la Categoría Actual

--- src/components/Categoria.jsx
+++ src/components/Categoria.jsx
@@ -1,10 +1,10 @@
 import useQuiosco from "../hooks/useQuiosco"
 
 export default function Categoria({ categoria }) {
-    const { handleClickCategoria } = useQuiosco();
+    const { handleClickCategoria, categoriaActual } = useQuiosco();
     const {icono,id,nombre} = categoria
     return (
-            <div className="flex items-center gap-4 border w-full p-3 hover:bg-amber-400 hover:text-white cursor-pointer">
+            <div className={`${categoriaActual.id === id ? "bg-amber-400 text-white" : "bg-white text-black" } flex items-center gap-4 border w-full p-3 hover:bg-amber-400 hover:text-white cursor-pointer `}>
                     <img 
                         alt="imagen icono"
                         src={`/img/icono_${icono}.svg`}
@@ -12,7 +12,7 @@ export default function Categoria({ categoria }) {
                         />
                         <button 
                             type="button"
-                            className="text-lg font-bold cursor-pointer truncate"
+                            className="text-lg font-bold cursor-pointer truncate w-full text-left"
                             onClick={ () => handleClickCategoria(id)}                    
                         > {nombre} </button>
                 </div>
