64e6a4d380b72102dd3b4900870db05eb1c5088e
Javier Dominguez Galindo <joshjavy@gmail.com>
2023-12-31 16:32:56 -0600

    332. Creando el Sidebar del Panel de Administración

--- src/components/AdminSideBar.jsx
+++ src/components/AdminSideBar.jsx
@@ -1,7 +1,33 @@
+import { Link } from 'react-router-dom'
+import { useAuth } from '../hooks/useAuth';
 export default function AdminSideBar() {
+    const { logout} = useAuth({middleware:'auth'});
   return (
     <aside className="md:w-72 h-screen">
-
+        <div className="p-4">
+        <img
+            className="w-40"
+            src="/img/logo.svg"
+            alt="Logo"
+        />
+        </div>
+        <nav className='flex flex-col px-4'>
+            <Link to='/admin'
+                className='font-bold text-lg'
+            > Ordenes </Link>
+            <Link to='/admin/productos'
+                className='font-bold text-lg'
+            > Productos </Link>
+        </nav>
+        <div className='my-5 px-5'>
+            <button
+            type='button'
+            className=" text-center bg-red-500 w-full p-3 font-bold text-white truncate"
+            onClick={()=>{
+                logout
+            }}
+            >Cerrar Sesion</button>
+        </div>
     </aside>
   )
 }
