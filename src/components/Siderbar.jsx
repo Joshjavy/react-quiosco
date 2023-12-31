b2c775e004431589aca00a3abf5f95db661b7723
Javier Dominguez Galindo <joshjavy@gmail.com>
2023-12-30 21:58:58 -0600

    323. Revocar un Token y cerrar la sesión

--- src/components/Siderbar.jsx
+++ src/components/Siderbar.jsx
@@ -1,8 +1,10 @@
 import useQuiosco from "../hooks/useQuiosco";
 import Categoria from "./Categoria"
+import { useAuth } from "../hooks/useAuth";
 
 export default function Siderbar() {
     const { categorias } = useQuiosco();
+    const { logout} = useAuth({middleware:'auth'});
     return (
         <aside className="md:w-72">
             <div className="p-4">
@@ -25,6 +27,7 @@ export default function Siderbar() {
                 <button 
                     type="button"
                     className=" text-center bg-red-500 w-full p-3 font-bold text-white truncate"
+                    onClick={()=>{logout()}}
                 >
                     Cancelar Orden
                 </button>
--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -34,8 +34,19 @@ export const useAuth=( {middleware, url})=>{
 
     }
 
-    const logout=()=>{
-
+    const logout= async ()=>{
+        try{
+            const { data } = await clienteAxios.post('/api/logout', null,{
+                headers:{
+                    Authorization:`Bearer ${token}`
+                }
+            });
+            localStorage.removeItem('AUTH_TOKEN');
+            await mutate(undefined);
+        }catch(error){
+            console.log(error);
+            setErrores( Object.values(error.response.data.errors) );
+        }
     }
 
     console.log(user)
