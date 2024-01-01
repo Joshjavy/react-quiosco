ce1fbf120ee839739b4afcb7f7849a284f7bbce7
Javier Dominguez Galindo <joshjavy@gmail.com>
2023-12-31 19:24:06 -0600

    338. Mostrando el Contenido del Pedido

--- src/views/Ordenes.jsx
+++ src/views/Ordenes.jsx
@@ -10,14 +10,38 @@ export default function Ordenes() {
       }
     })
 
-  const { data, error, isLogin } =useSwr ('/api/pedidos',fetcher)
-  console.log(data);
-  console.log(error);
-  console.log(isLogin);
+  //const { data, error, isLogin } =useSwr ('/api/pedidos',fetcher,{ refreshInterval: 1000})
+  const { data, error, isLoading } =useSwr ('/api/pedidos',fetcher)
+  if(isLoading) return 'Cargando...';
   return (
     <>
       <h1 className='text-4xl font-black'> Ordenes</h1>
       <p className='text-2xl my-10'> Administra las ordenes desde aqui</p>
+
+      <div className=''>
+       {
+        data.data.data.map(pedido => (
+          <div key={pedido.id}
+                className='p-5 bg-white shadow space-y-2 border-b'
+            >
+              <p className='text-xl font-bold text-slate-600'>
+                  Contenido del pedido: 
+              </p>
+              {
+              pedido.productos.map( producto =>(
+                  <div 
+                    key={ producto.id}
+                    className='border-b border-b-slate-200 last-of-type:border-none py-2'
+                  >
+                    <p className='text-sm'> ID: {producto.id}</p>
+                    <p className='text-sm'>{producto.nombre}</p>
+                    <p className='text-sm'>Cantidad: {''} <span className='font-bold'> { producto.pivot.cantidad } </span></p>
+                  </div>
+                ))
+              }
+          </div>
+        ))}
+      </div>
     </>
   )
 }
